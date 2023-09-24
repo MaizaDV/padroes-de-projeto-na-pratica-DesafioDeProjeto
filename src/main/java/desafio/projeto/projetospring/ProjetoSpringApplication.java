cfa19d32aee29fe1998f8576a4dd2056d65f3d4b
maiza <021maizadv@gmail.com>
2023-09-23 21:15:16 -0300

    :package: cria estrutura inicial de pacotes

--- src/main/java/desafio/projeto/projetospring/ProjetoSpringApplication.java
+++ src/main/java/desafio/projeto/projetospring/ProjetoSpringApplication.java
@@ -2,12 +2,14 @@ package desafio.projeto.projetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProjetoSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSpringApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetoSpringApplication.class, args);
+  }
 
 }
